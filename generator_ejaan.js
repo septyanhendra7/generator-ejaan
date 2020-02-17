717b6de27957135c7344f04515a7c46ce62824f1
septyanhendra7 <septyanhendra7@gmail.com>
2020-02-17 10:00:01 +0700

    perbaikan fungsi js

--- generator_ejaan.js
+++ generator_ejaan.js
@@ -1,6 +1,7 @@
 function kalimat_vo() {
-    var vo = document.getElementById("kalimat1");
-    var ganti_vo = vo.replace(/a|i|u|e|o/gi,"i");
+    var vo = document.getElementById("kalimat1").value;
+    var ganti_vo = vo.replace(/a|i|u|e|o/gi,'i');
     document.getElementById("kalimat2").innerHTML = ganti_vo;
     document.getElementById("tes").innerHTML = vo;
 }
+
