487657b45dd84af56f23e5a4922984dbae1ab2cc
septyanhendra7 <septyanhendra7@gmail.com>
2020-02-18 13:58:48 +0700

    edit file html dan js

--- generator_ejaan.js
+++ generator_ejaan.js
@@ -1,7 +1,16 @@
 function kalimat_vo() {
     var vo = document.getElementById("kalimat1").value;
-    var ganti_vo = vo.replace(/a|i|u|e|o/gi,'i');
-    document.getElementById("kalimat2").innerHTML = ganti_vo;
-    document.getElementById("tes").innerHTML = vo;
+    //var ganti_vo = vo.replace(/a|i|u|e|o/gi,'i');
+    var ganti_vo = /(u).*(y)/;
+
+    var a_vo = ganti_vo.exec(vo);
+
+    document.getElementById("vo").innerHTML = a_vo[0];
+    document.getElementById("vo").innerHTML = a_vo[1];
+
+    vo = vo.replace(a_vo[0],'oe');
+    vo = vo.replace(a_vo[1],'j');
+    document.getElementById("kalimat2").innerHTML = vo;
+    document.getElementById("tes").innerHTML = a_vo;
 }
 
