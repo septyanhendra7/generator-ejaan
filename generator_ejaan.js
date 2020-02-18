85d898b42c831889f994940589f04f8863895ab6
septyanhendra7 <septyanhendra7@gmail.com>
2020-02-18 14:19:07 +0700

    edit berhasil

--- generator_ejaan.js
+++ generator_ejaan.js
@@ -1,16 +1,5 @@
-function kalimat_vo() {
+function proses(){
     var vo = document.getElementById("kalimat1").value;
-    //var ganti_vo = vo.replace(/a|i|u|e|o/gi,'i');
-    var ganti_vo = /(u).*(y)/;
-
-    var a_vo = ganti_vo.exec(vo);
-
-    document.getElementById("vo").innerHTML = a_vo[0];
-    document.getElementById("vo").innerHTML = a_vo[1];
-
-    vo = vo.replace(a_vo[0],'oe');
-    vo = vo.replace(a_vo[1],'j');
-    document.getElementById("kalimat2").innerHTML = vo;
-    document.getElementById("tes").innerHTML = a_vo;
+    var ganti = vo.replace("u","oe").replace("c","tj").replace("y","j");
+    document.getElementById("kalimat2").innerHTML = ganti;
 }
-
